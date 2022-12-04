c963b69adc6421184e34c8efcba686145faa0246
AKEJU FATAI <akejufatai@yahoo.com>
2022-12-04 13:05:43 +0100

    [chore] setup back-end basic-app

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,1 +1,21 @@
-console.log("Awesome!!!");
+import App from "./app";
+import express from "express";
+import config from "./config";
+
+/********************************************************
+ * APPLICATION MAIN
+ ********************************************************/
+
+const main = async () => {
+  const app = new App(express(), Number(config.PORT));
+  await app.initialize();
+  app.checkDependencies();
+
+  app.run();
+};
+
+/********************************************************
+ * RUN APPLICATION
+ ********************************************************/
+
+main().catch(console.log);
