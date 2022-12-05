4c2bfa15146c06f36d5b40b47adf5637d0677a9e
AKEJU FATAI <akejufatai@yahoo.com>
2022-12-05 06:11:14 +0100

    [feat] get user-profile

---
+++ back-end/src/controllers/user.controller.ts
@@ -0,0 +1,25 @@
+import { Inject, Service } from "typedi";
+import { Controller } from "../decorators";
+import { Request, Response } from "express";
+import { ResponseHandler } from "../helpers";
+import { UserService } from "../services/user.service";
+
+@Service()
+@Controller()
+export class UserController {
+  // eslint-disable-next-line no-useless-constructor
+  constructor(@Inject() private userService: UserService) {}
+
+  /**
+   * @method getProfile
+   * @async
+   * @param {Request} req
+   * @param {Response} res
+   */
+  async getProfile(req: Request, res: Response) {console.log(req.auth)
+    const USER = await this.userService.getUser(req.auth?.userId as string);
+
+    ResponseHandler.ok(res, USER);
+  }
+
+}
---
+++ back-end/src/middlewares/require-auth.middleware.ts
@@ -0,0 +1,27 @@
+import { Role } from "../models";
+import { verifyAuthToken } from "../helpers";
+import { IAuthTokenPayload } from "../interfaces";
+import { UnauthorizedError } from "../exceptions";
+import { NextFunction, Request, Response } from "express";
+
+/**
+ * @function requireAuth
+ * @param {Array<Role>} allowedRoles 
+ * @returns {Function}
+ */
+export const requireAuth = (allowedRoles?: Array<Role>) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const authTokenPayload: IAuthTokenPayload = verifyAuthToken(req.headers);
+
+        if(!allowedRoles || allowedRoles.includes(authTokenPayload.role)) {
+          req.auth = authTokenPayload;
+          return next();
+        }
+
+        next(new UnauthorizedError("Access denied!"));
+      } catch(err) {
+          next(err);
+      }
+    }
+}
