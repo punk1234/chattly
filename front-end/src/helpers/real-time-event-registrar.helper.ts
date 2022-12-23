233fffbc40672606f799f1cf11d0f0334268d3d6
AKEJU FATAI <akejufatai@yahoo.com>
2022-12-23 12:51:19 +0100

    fix lint

--- front-end/src/helpers/real-time-event-registrar.helper.ts
+++ front-end/src/helpers/real-time-event-registrar.helper.ts
@@ -4,42 +4,47 @@ import { IRealTimeEventHandlers, IRTNewMessagePayload } from "../interfaces";
  * @class RealTimeEventRegistrar
  */
 export class RealTimeEventRegistrar {
-
-  static sync(webSocketHost: string, username: string, eventHandlers: IRealTimeEventHandlers): void {
+  static sync(
+    webSocketHost: string,
+    username: string,
+    eventHandlers: IRealTimeEventHandlers,
+  ): void {
     const webSocket = new WebSocket(`${webSocketHost}/${username}`);
 
     webSocket.onmessage = (evt: MessageEvent<IRTNewMessagePayload>): void => {
       // TODO: HANDLE ON-MESSAGE EVENT
       // IF `evt.data.type === "MSG"` ELSE IF ``evt.data.type === "NGC"` ELSE `IGNORE`
       eventHandlers.onNewChatMessageHandler(evt.data);
-    }
+    };
 
+    // eslint-disable-next-line
     webSocket.onerror = (evt: Event): void => {
       // TODO: HANDLE ON-ERROR EVENT
-    }
+    };
 
+    // eslint-disable-next-line
     webSocket.onopen = (evt: Event): void => {
       // TODO: HANDLE ON-OPEN EVENT
-    }
+    };
 
+    // eslint-disable-next-line
     webSocket.onclose = (evt: CloseEvent): void => {
       // TODO: HANDLE ON-CLOSE EVENT
-    }
+    };
 
-    console.log("+++++")
+    console.log("+++++");
 
     // NOTE: TESTING ON-MESSAGE EVENT
     setInterval(() => {
       console.log("---> GOT HERE 5SEC(S)!!!");
-      console.log(eventHandlers.onNewChatMessageHandler)
+      console.log(eventHandlers.onNewChatMessageHandler);
 
       eventHandlers.onNewChatMessageHandler({
         chatId: "9de37e78-24a8-4910-a0b8-256a4d0a00dc",
         message: "Hmmm, testing websocket from front-end...!",
         messageAt: new Date().toISOString(),
-        sender: "abcd"
+        sender: "abcd",
       });
     }, 5000);
   }
-
 }
--- front-end/src/interfaces/real-time-event-handlers.interface.ts
+++ front-end/src/interfaces/real-time-event-handlers.interface.ts
@@ -2,24 +2,24 @@
  * @interface IRTNewMessagePayload
  */
 export interface IRTNewMessagePayload {
-  chatId: string,
-  message: string,
-  messageAt: string,
-  sender: string,
+  chatId: string;
+  message: string;
+  messageAt: string;
+  sender: string;
   // NOTE: SENDER IMAGE-URL TO BE IGNORED FOR NOW
-  imageURL?: string,
+  imageURL?: string;
 }
 
 /**
  * @interface IRTNewGroupChatPayload
  */
 export interface IRTNewGroupChatPayload {
-  chatId: string,
-  name: string,
-  imageURL: string,
-  createdBy: string,
-  message: string,
-  messageAt: string
+  chatId: string;
+  name: string;
+  imageURL: string;
+  createdBy: string;
+  message: string;
+  messageAt: string;
 }
 
 /**
